a931b48ac889e2e72e4e0fcc363242a1ef868896
Anna Doubkova <doubkova.anna@gmail.com>
2019-05-27 12:05:42 +0100

    Remove flow

--- App.js
+++ App.js
@@ -1,16 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
     render() {
         return (
             <View style={styles.container}>
